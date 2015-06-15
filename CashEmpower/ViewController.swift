df8480f4d3f1660a4be258c08e695e294f50b77a
Noah Antony <nayagam1989@gmail.com>
2015-06-15 22:34:37 +0800

    Issue#1 - Some changes in login page.

--- CashEmpower/ViewController.swift
+++ CashEmpower/ViewController.swift
@@ -27,19 +27,35 @@ class ViewController: UIViewController {
         var textUserName = "administrator"
         var textPassword = "Password123"
         
-        if(userName.text == textUserName && password.text == textPassword)
+/*        if(userName.text == textUserName && password.text == textPassword)
         {
-            let alertController = UIAlertController(title: "Login Successful", message: "Username and password is correct", preferredStyle: UIAlertControllerStyle.Alert)
+            let alertController = UIAlertController(title: "Login Successful", message: "Username and password are correct", preferredStyle: UIAlertControllerStyle.Alert)
             alertController.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.Default,handler:nil))
             self.presentViewController(alertController, animated: true, completion: nil)
+            
+        } */
+        
+        
+        if(userName.text == textUserName && password.text == textPassword)
+        {
+            let alertController = UIAlertController(title: "Login Successful", message: "Username and password are correct", preferredStyle: UIAlertControllerStyle.Alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel ){ (action) in }
+            alertController.addAction(cancelAction)
+            let OKAction = UIAlertAction(title: "Continue", style: .Default) { (action) in
+                self.performSegueWithIdentifier("HomePage", sender: self)}
+            alertController.addAction(OKAction)
+            presentViewController(alertController, animated: true, completion: nil)
+            
+            //alertController.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.Default,handler:nil))
+            //self.presentViewController(alertController, animated: true, completion: nil)
+            
         }
         else
         {
-            let alertController = UIAlertController(title: "Login Failed", message: "Username and password is incorrect", preferredStyle: UIAlertControllerStyle.Alert)
-            alertController.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.Default,handler:nil))
+            let alertController = UIAlertController(title: "Login Failed", message: "Username and password are incorrect", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default,handler:nil))
             self.presentViewController(alertController, animated: true, completion: nil)
         }
         
     }
 }
-
